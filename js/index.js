b077082bfc998d688d0ee05cb49bdba38ba33e3c
刘洋 <liuyang@obencn.com>
2019-11-20 10:17:52 +0800

    V1.0

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log($);
+$(function(){
+  var tableData;
+  
+  $.ajax({
+    url:'./speed.json',
+    async:false,
+    success:function(res){
+      console.log(res);
+      tableData = res
+
+    }
+  })
+
+  var htmlStr = template('tb',{data:tableData})
+  console.log(htmlStr);
+  $('tbody').html(htmlStr)
+  
+  
+})
