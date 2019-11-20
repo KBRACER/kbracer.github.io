9c9f85076a6fcd74b64c259e470ec284ea996a8b
刘洋 <liuyang@obencn.com>
2019-11-20 13:56:33 +0800

    V1.0.1

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 $(function(){
   var tableData;
-  
+  template.defaults.imports.timeFormat = function(date){
+    var timeArr = date.split('.')
+    ,sec = timeArr[0] % 60
+    ,min
+    timeArr[0] >= 60 ? min = 1: min = 0
+    sec < 10 ? sec = '0'+sec:null
+    return `${min}：${sec}.${timeArr[1]}`
+  }
   $.ajax({
     url:'./speed.json',
     async:false,
     success:function(res){
-      console.log(res);
+      // console.log(res);
       tableData = res
-
     }
   })
 
   var htmlStr = template('tb',{data:tableData})
-  console.log(htmlStr);
   $('tbody').html(htmlStr)
   
   
