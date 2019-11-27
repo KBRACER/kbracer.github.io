7890cda38d0d7ed38a17a8b605a92c2fc5bc00a8
刘洋 <liuyang@obencn.com>
2019-11-27 16:50:19 +0800

    更新UI

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(function () {
     var htmlStr = template('tb', {
       data: arr
     })
-    $('tbody').html(htmlStr)
+    $("#tb_results > tbody").html(htmlStr)
   }
 
   template.defaults.imports.timeFormat = function (date) {
@@ -14,7 +14,7 @@ $(function () {
       min
     timeArr[0] >= 60 ? min = 1 : min = 0
     sec < 10 ? sec = '0' + sec : null
-    return `${min}：${sec}.${timeArr[1]}`
+    return `${min}:${sec}.${timeArr[1]}`
   }
   //获取json数据
   $.ajax({
@@ -50,7 +50,6 @@ $(function () {
       return v.car.toLowerCase().indexOf(val) != -1
     })
     setTable(arr)
-    
   })
 
   console.log('%c键盘车神教', "text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb, 0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,0.1),0 0 5px rgba(0,0,0,0.25),0 10px 10px rgba(0,0,0,0.2),0 20px 20px rgba(0,0,0,0.15);font-size:100px");
