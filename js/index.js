5f6514c120d43e23a09afd5911c910c3f823463d
刘洋 <liuyang@obencn.com>
2019-12-04 13:42:26 +0800

    轮胎标注

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ $(function () {
   }
   //获取json数据
   $.ajax({
-    url: './speed.json?v=201912021706',
+    url: './speed.json?v=20191241341',
     async: false,
     success: function (res) {
       // console.log(res);
