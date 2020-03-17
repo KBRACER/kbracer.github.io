1c325d951e4784fa07787a66955c889e984edeeb
刘洋 <liuyang@obencn.com>
2020-03-17 19:29:55 +0800

    golf

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ $(function () {
   }
   //获取json数据
   $.ajax({
-    url: './speed.json?v=202003171703',
+    url: './speed.json?v=202003171929',
     async: false,
     success: function (res) {
       // console.log(res);
